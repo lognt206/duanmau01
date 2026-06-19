c53a885f6d6aaaf07bbffd2def221901fa672aaa
lognt206 <longgt206@gmail.com>
2026-06-19 19:52:26 +0700

    view

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -5,22 +5,40 @@ class BaseModel
     protected $table;
     protected $pdo;
 
-    // Kết nối CSDL
     public function __construct()
     {
-        $dsn = sprintf('mysql:host=%s;port=%s;dbname=%s;charset=utf8', DB_HOST, DB_PORT, DB_NAME);
+        $dsn = sprintf(
+            'mysql:host=%s;port=%s;dbname=%s;charset=utf8',
+            DB_HOST,
+            DB_PORT,
+            DB_NAME
+        );
 
         try {
             $this->pdo = new PDO($dsn, DB_USERNAME, DB_PASSWORD, DB_OPTIONS);
         } catch (PDOException $e) {
-            // Xử lý lỗi kết nối
-            die("Kết nối cơ sở dữ liệu thất bại: {$e->getMessage()}. Vui lòng thử lại sau.");
+            die("Kết nối cơ sở dữ liệu thất bại: {$e->getMessage()}");
         }
     }
 
-    // Hủy kết nối CSDL
     public function __destruct()
     {
         $this->pdo = null;
+    }
+
+    public function getAll($table)
+    {
+        $sql = "SELECT * FROM $table";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
+    public function find($table, $id)
+    {
+        $sql = "SELECT * FROM $table WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(['id' => $id]);
+        return $stmt->fetch();
     }
 }
