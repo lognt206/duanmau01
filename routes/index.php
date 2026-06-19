0a6b4c9fd875c426dcbb797ea6db1f7a18e26cb0
lognt206 <longgt206@gmail.com>
2026-06-19 20:48:42 +0700

    client

--- routes/index.php
+++ routes/index.php
@@ -4,8 +4,8 @@ $act = $_GET['act'] ?? '/';
 
 match ($act) {
     '/' => (new HomeController())->index(),
+    'products' => (new HomeController())->products(),
     'detail' => (new HomeController())->detail(),
-
     'admin-products' => (new HomeController())->adminProducts(),
 
     default => die('404 - Không tìm thấy trang'),
